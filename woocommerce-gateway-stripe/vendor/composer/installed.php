b84e4c46c42ea83b116eec50941a73b298bc5de1
Sean Murphy <sean@iamseanmurphy.com>
2026-08-05 17:51:32 +0000

    10.8.5

--- woocommerce-gateway-stripe/vendor/composer/installed.php
+++ woocommerce-gateway-stripe/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'woocommerce/woocommerce-gateway-stripe',
-        'pretty_version' => 'dev-release/10.8.4',
-        'version' => 'dev-release/10.8.4',
-        'reference' => 'c9ec847273042aa3bcda014fcf05e717b35627a2',
+        'pretty_version' => 'dev-release/10.8.5',
+        'version' => 'dev-release/10.8.5',
+        'reference' => 'bfff4c861a54e67fa7dc7fb7bfb454bcf63e5901',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'woocommerce/woocommerce-gateway-stripe' => array(
-            'pretty_version' => 'dev-release/10.8.4',
-            'version' => 'dev-release/10.8.4',
-            'reference' => 'c9ec847273042aa3bcda014fcf05e717b35627a2',
+            'pretty_version' => 'dev-release/10.8.5',
+            'version' => 'dev-release/10.8.5',
+            'reference' => 'bfff4c861a54e67fa7dc7fb7bfb454bcf63e5901',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
