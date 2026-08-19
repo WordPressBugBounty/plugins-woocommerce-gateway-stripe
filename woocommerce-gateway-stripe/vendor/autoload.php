180a1061d7e60cb070708b00f1e4e06662054062
Sean Murphy <sean@iamseanmurphy.com>
2026-08-19 15:41:53 +0000

    10.9.0

--- woocommerce-gateway-stripe/vendor/autoload.php
+++ woocommerce-gateway-stripe/vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit93a6c12dcd489fbadaeec550274b5ed0::getLoader();
+return ComposerAutoloaderInit7914bea8bfd344fbc96257f6b02db22c::getLoader();
--- woocommerce-gateway-stripe/vendor/composer/installed.php
+++ woocommerce-gateway-stripe/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'woocommerce/woocommerce-gateway-stripe',
-        'pretty_version' => 'dev-release/10.8.5',
-        'version' => 'dev-release/10.8.5',
-        'reference' => 'bfff4c861a54e67fa7dc7fb7bfb454bcf63e5901',
+        'pretty_version' => 'dev-release/10.9.0',
+        'version' => 'dev-release/10.9.0',
+        'reference' => '836a0cfe6a6f2e574d1d8dd8e6604a8ad1428bbe',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'woocommerce/woocommerce-gateway-stripe' => array(
-            'pretty_version' => 'dev-release/10.8.5',
-            'version' => 'dev-release/10.8.5',
-            'reference' => 'bfff4c861a54e67fa7dc7fb7bfb454bcf63e5901',
+            'pretty_version' => 'dev-release/10.9.0',
+            'version' => 'dev-release/10.9.0',
+            'reference' => '836a0cfe6a6f2e574d1d8dd8e6604a8ad1428bbe',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
